a9c2772ce0324aaebe85bdd1a5a0a87aecb6445d
GersoZz <37023982+GersoZz@users.noreply.github.com>
2025-11-04 20:32:51 -0500

    feat: add example Terraform security group configurations

---
+++ infra/terraform/example.tf
@@ -0,0 +1,23 @@
+# Ejemplo vulnerable (inseguro) y ejemplo bueno
+
+# --- grupo de seguridad vulnerable (inseguro) ---
+resource "aws_security_group" "bad-SSH" {
+  name = "Bad_SSH" # nombre incorrecto (mayúsculas y guion bajo)
+  ingress {
+    from_port = 22
+    to_port   = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # secreto incluido accidentalmente:
+  aws_access_key_id = "AKIAEXAMPLEKEY1234" 
+}
+
+# --- buen ejemplo ---
+resource "aws_security_group" "good-ssh" {
+  name = "good-ssh"
+  ingress {
+    from_port = 443
+    to_port   = 443
+    cidr_blocks = ["10.0.0.0/24"]
+  }
+}
